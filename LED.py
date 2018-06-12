073e32dec7f5ef58810ff8945b4ca8bf82ff0674
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-06-12 15:16:25 +0200

    Update LED.py

--- LED.py
+++ LED.py
@@ -51,6 +51,11 @@ def check():
         bus.write_byte_data(adress_22,0x0c,0xff)
         bus.write_byte_data(adress_23,0x0d,0xff)
         bus.write_byte_data(adress_23,0x0c,0xff)
+        
+        bus.write_byte_data(adress_24,OLATA,0xff)
+        bus.write_byte_data(adress_24,OLATB,0xff)
+        bus.write_byte_data(adress_25,OLATA,0xff)
+        bus.write_byte_data(adress_25,OLATB,0xff)
     
         for out in range(0,4):
 
