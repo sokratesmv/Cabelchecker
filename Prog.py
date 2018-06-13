448799b38c805530f11fc67d3540d88a0cc9750f
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-06-13 10:25:16 +0200

    Update Prog.py

--- Prog.py
+++ Prog.py
@@ -10,7 +10,6 @@ import L123R123
 import LR
 import PW
 import LED
-import reset
 
 #bus = smbus.SMBus(0)
 bus = smbus.SMBus(1)
@@ -57,8 +56,6 @@ class MyFirstGuiProgram(Ui_Form):
         self.reset()
         
     def reset(self):
-        
-        reset
 
         for i in range(0,6):
             for a in range(1,8):
