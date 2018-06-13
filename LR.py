2944e07c630c131c55055ce0c26558eb1b5d4198
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-06-13 16:09:35 +0200

    Update LR.py

--- LR.py
+++ LR.py
@@ -21,44 +21,45 @@ OLATB  = 0x15
 GPIOA  = 0x12 
 GPIOB  = 0x13
 
-LR_write = [4,5,6,12,13,14,20,21,22,28,29,30,36,37,38,44,45,46]
-LR_read = [67,66,65,70,69,68,87,72,71,75,74,73,78,77,76,88,80,79]
-LR_in = [4,2,1,32,16,8,64,128,64,4,2,1,32,16,8,128,128,64,]
+LR_read = [4,5,6,12,13,14,20,21,22,28,29,30,36,37,38,44,45,46]
+LR_write = [67,66,65,70,69,68,87,72,71,75,74,73,78,77,76,88,80,79]
+LR_in = [8,16,32,8,16,32,8,16,32,8,16,32,8,16,32,8,16,32]
 LR_result = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 #check if cable is OK
 def check():
         
+        #setting pins as outputs
+        bus.write_byte_data(adress_24,IO_DIR_A,0x00)
+        bus.write_byte_data(adress_24,IO_DIR_B,0x00)
+        bus.write_byte_data(adress_25,IO_DIR_A,0x00)
+        bus.write_byte_data(adress_25,IO_DIR_B,0x00)
+        #setting pins as pull-up inputs
+        bus.write_byte_data(adress_20,IO_DIR_A,0xff)
+        bus.write_byte_data(adress_20,IO_DIR_B,0xff)
+        bus.write_byte_data(adress_21,IO_DIR_A,0xff)
+        bus.write_byte_data(adress_21,IO_DIR_B,0xff)
+        bus.write_byte_data(adress_22,IO_DIR_A,0xff)
+        bus.write_byte_data(adress_22,IO_DIR_B,0xff)
+        bus.write_byte_data(adress_23,IO_DIR_A,0xff)
+        bus.write_byte_data(adress_23,IO_DIR_B,0xff)
+        bus.write_byte_data(adress_20,0x0d,0xff)
+        bus.write_byte_data(adress_20,0x0c,0xff)
+        bus.write_byte_data(adress_21,0x0d,0xff)
+        bus.write_byte_data(adress_21,0x0c,0xff)
+        bus.write_byte_data(adress_22,0x0d,0xff)
+        bus.write_byte_data(adress_22,0x0c,0xff)
+        bus.write_byte_data(adress_23,0x0d,0xff)
+        bus.write_byte_data(adress_23,0x0c,0xff)
+
+                        
+        bus.write_byte_data(adress_24,OLATA,0xff)
+        bus.write_byte_data(adress_24,OLATB,0xff)
+        bus.write_byte_data(adress_25,OLATA,0xff)
+        bus.write_byte_data(adress_25,OLATB,0xff)
+        
         for out in range(0,18):
                 
-                #setting pins as outputs
-                bus.write_byte_data(adress_20,IO_DIR_A,0x00)
-                bus.write_byte_data(adress_20,IO_DIR_B,0x00)
-                bus.write_byte_data(adress_21,IO_DIR_A,0x00)
-                bus.write_byte_data(adress_21,IO_DIR_B,0x00)
-                bus.write_byte_data(adress_22,IO_DIR_A,0x00)
-                bus.write_byte_data(adress_22,IO_DIR_B,0x00)
-                bus.write_byte_data(adress_23,IO_DIR_A,0x00)
-                bus.write_byte_data(adress_23,IO_DIR_B,0x00)
-                #setting pins as pull-up inputs
-                bus.write_byte_data(adress_24,IO_DIR_A,0xff)
-                bus.write_byte_data(adress_24,IO_DIR_B,0xff)
-                bus.write_byte_data(adress_25,IO_DIR_A,0xff)
-                bus.write_byte_data(adress_25,IO_DIR_B,0xff)
-                bus.write_byte_data(adress_24,0x0d,0xff)
-                bus.write_byte_data(adress_24,0x0c,0xff)
-                bus.write_byte_data(adress_25,0x0d,0xff)
-                bus.write_byte_data(adress_25,0x0c,0xff)
-                
-                bus.write_byte_data(adress_20,OLATA,0xff)
-                bus.write_byte_data(adress_20,OLATB,0xff)
-                bus.write_byte_data(adress_21,OLATA,0xff)
-                bus.write_byte_data(adress_21,OLATB,0xff)
-                bus.write_byte_data(adress_22,OLATA,0xff)
-                bus.write_byte_data(adress_22,OLATB,0xff)
-                bus.write_byte_data(adress_23,OLATA,0xff)
-                bus.write_byte_data(adress_23,OLATB,0xff)
-
                 adress_in,port_in,pin_in = IO.read_pin(LR_read[out])
                 adress_out,port_out,pin_out = IO.write_pin(LR_write[out])
                         
