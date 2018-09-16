65e98bf7b469abb5ff4d063a90ed105955e9a990
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-09-16 22:35:24 +0200

    Update LED.py

--- LED.py
+++ LED.py
@@ -3,25 +3,6 @@ import smbus
 import time
 import IO
 
-#bus = smbus.SMBus(0)
-bus = smbus.SMBus(1)
-# Device address
-adress_20  = 0x20 
-adress_21 = 0x21
-adress_22 = 0x22
-adress_23 = 0x23
-adress_24 = 0x24
-adress_25 = 0x25
-# Pin direction register
-IO_DIR_A = 0x00 
-IO_DIR_B = 0x01
-# Register for outputs
-OLATA  = 0x14 
-OLATB  = 0x15
-# Register for inputs
-GPIOA  = 0x12 
-GPIOB  = 0x13
-
 bus = smbus.SMBus(1)
 # Device address
 adress_20  = 0x20 
