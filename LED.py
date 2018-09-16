c99ff7bdce325719e448f7b003c9fcddc60484bf
Hammerboy1 <ingar.hjelle99@gmail.com>
2018-09-16 22:33:16 +0200

    Update LED.py

--- LED.py
+++ LED.py
@@ -61,7 +61,7 @@ def check():
         bus.write_byte_data(adress_22,IO_DIR_A,0xff)
         bus.write_byte_data(adress_22,IO_DIR_B,0xff)
         bus.write_byte_data(adress_23,IO_DIR_A,0xff)
-        bus.write_byte_data(adress_23,IO_DIR_B,0x00)
+        bus.write_byte_data(adress_23,IO_DIR_B,0xff)
         bus.write_byte_data(adress_20,0x0d,0xff)
         bus.write_byte_data(adress_20,0x0c,0xff)
         bus.write_byte_data(adress_21,0x0d,0xff)
@@ -69,13 +69,12 @@ def check():
         bus.write_byte_data(adress_22,0x0d,0xff)
         bus.write_byte_data(adress_22,0x0c,0xff)
         bus.write_byte_data(adress_23,0x0d,0xff)
-        #bus.write_byte_data(adress_23,0x0c,0xff)
+        bus.write_byte_data(adress_23,0x0c,0xff)
         
         bus.write_byte_data(adress_24,OLATA,0xff)
         bus.write_byte_data(adress_24,OLATB,0xff)
         bus.write_byte_data(adress_25,OLATA,0xff)
         bus.write_byte_data(adress_25,OLATB,0xff)
-        bus.write_byte_data(adress_23,OLATB,0xff)
     
         for out in range(0,4):
 
