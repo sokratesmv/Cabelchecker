5df3e728d6631f8346bfac8541ac9c35e60832f7
Hammerboy1 <ingar.hjelle99@gmail.com>
2019-06-01 16:06:25 +0200

    Update ROOF.py

--- ROOF.py
+++ ROOF.py
@@ -72,9 +72,9 @@ def check():
                         ROOF_result[out] = 1
                 if read != LR_in[out]:
                         ROOF_result[out] = 0
-                print ("LR_write-->", LR_write[out], "LR_read-->", LR_read[out], "read-->", read, "result-->", ROOF_result[out])
+                #print ("LR_write-->", LR_write[out], "LR_read-->", LR_read[out], "read-->", read, "result-->", ROOF_result[out])
 
                 out = out + 1
                 
         return(ROOF_result)
-check()
+#check()
